93d4e0b7c5f3d39ac0d0cfaa855726f9c95f2cc7
Ma Catherine Masinloc <mcgmasinloc@gmail.com>
2021-07-21 10:07:27 +0800

    Fix spacings

--- src/components/LinearScale.js
+++ src/components/LinearScale.js
@@ -1,23 +1,29 @@
 const LinearScale = ({question, onChange}) => {
   return (
     <div className="linear-scale-container">
-      <div
-        className="rate-experience-score-start rate-experience-score"
-      >
-        1<br/>
+      <div className="rate-experience-score-start rate-experience-score">
+        1
+        <br/>
         <input type="radio" name={question.id} onChange={onChange} value="1" />
       </div>
       <div className="rate-experience-score">
-        2<br/><input type="radio" name={question.id} onChange={onChange} value="2" />
+        2
+        <br/>
+        <input type="radio" name={question.id} onChange={onChange} value="2" />
       </div>
       <div className="rate-experience-score">
-        3<br/><input type="radio" name={question.id} onChange={onChange} value="3" />
+        3<br/>
+        <input type="radio" name={question.id} onChange={onChange} value="3" />
       </div>
       <div className="rate-experience-score">
-        4<br/><input type="radio" name={question.id} onChange={onChange} value="4" />
+        4
+        <br/>
+        <input type="radio" name={question.id} onChange={onChange} value="4" />
       </div>
       <div className="rate-experience-score-end rate-experience-score">
-        5<br/><input type="radio" name={question.id} onChange={onChange} value="5" />
+        5
+        <br/>
+        <input type="radio" name={question.id} onChange={onChange} value="5" />
       </div>
     </div>
   )
