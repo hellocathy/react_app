76d44c763b894cd90c69938ecc6dace92e1571c5
Ma Catherine Masinloc <mcgmasinloc@gmail.com>
2021-07-21 09:13:52 +0800

    Refactor

--- src/components/HelpImprove.js
+++ src/components/HelpImprove.js
@@ -1,22 +1,12 @@
-import MovingComponent from 'react-moving-text'
-
-const HelpImprove = ({onMouseOver, onMouseOut, onClick}) => {
+const HelpImprove = ({onMouseOut, onClick}) => {
   return (
-    <MovingComponent
-      className="help-improve"
-      onMouseLeave={onMouseOut}
-      onClick={onClick}
-      type="fadeInFromLeft"
-      duration="500ms"
-      delay="0s"
-      direction="normal"
-      timing="ease"
-      iteration="1"
-      fillMode="none">
-        <div className="help-improve-text">
-          Help us improve
-        </div>
-    </MovingComponent>
+    <div className="help-improve"
+    onMouseLeave={onMouseOut}
+    onClick={onClick}>
+      <div className="help-improve-text">
+        Help us improve
+      </div>
+    </div>
   )
 }
 
